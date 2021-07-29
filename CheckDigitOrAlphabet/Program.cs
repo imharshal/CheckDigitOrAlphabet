09ce1fa9f8f6e66be17856b41de4017a6095addc
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 20:33:18 +0530

    Add project files.

---
+++ CheckDigitOrAlphabet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CheckDigitOrAlphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.WriteLine("Enter a character");
+            str = Console.ReadLine();
+            char ch = str[0];
+            if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) { 
+                Console.WriteLine("Alphabet");
+            }
+            else if (ch >= 48 && ch <= 57) { 
+                Console.WriteLine("Digit");
+            }
+        }
+    }
+}
